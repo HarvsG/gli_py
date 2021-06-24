87fc38db0dcbb865ead806538f6c2469feb40b1d
HarvsG <11440490+HarvsG@users.noreply.github.com>
2021-06-24 16:22:45 +0100

    refactor

--- gli_py/__init__.py
+++ gli_py/__init__.py
@@ -1,37 +1,4 @@
-from uplink import Consumer, get, post, Query, headers, returns, response_handler #, Path, Field
-#import cache
-from error_handling import raise_for_status
-
-
-# typical base url http://192.168.8.1/cgi-bin/api/
-class GLinet(Consumer):
-    """A Python Client for the GL-inet API."""
-
-    def __init__(self, base_url : str, password : str):
-        #initialise the super class
-        super(GLinet, self).__init__(base_url=base_url)
-
-        # use the token for auth for all requests henceforth
-        self.session.headers["Authorization"] = self.login(password)
-
-    @response_handler(raise_for_status)
-    @returns.json(key="token")
-    @post("router/login")
-    def login(self, pwd: Field):
-        """fetches token"""
-        # TODO deal with errors
-
-    #@cache(hours=3)
-    @returns.json(key="model")
-    @get("router/model")
-    def router_model(self):
-        """Retrieves the router's model, no auth required"""
-
-    @response_handler(raise_for_status)
-    @returns.json(key="mac")
-    @get("router/mac/get")
-    def router_mac(self):
-        """Retrieves the router's model"""
+from glinet import GLinet
 
 # for dev only
 if __name__=="__main__":
