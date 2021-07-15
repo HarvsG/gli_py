40200f4656b7a897f0485881e7a002a4bfc68bd0
HarvsG <11440490+HarvsG@users.noreply.github.com>
2021-07-15 16:54:04 +0100

    update example code

--- gli_py/__init__.py
+++ gli_py/__init__.py
@@ -13,7 +13,7 @@ if __name__=="__main__":
     my_router = GLinet(pwd, base_url="http://192.168.0.1/cgi-bin/api/")
 
     # async version
-    #my_router = GLinet(pwd, base_url="http://192.168.0.1/cgi-bin/api/", client=AiohttpClient())
+    #my_router = GLinet(pwd, base_url="http://192.168.0.1/cgi-bin/api/", sync=False)
 
     # get mac (requires auth)
     print(my_router.wan_ip())
