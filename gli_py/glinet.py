1fbbf586751668aace79b36034e4ddf0104b3439
HarvsG <11440490+HarvsG@users.noreply.github.com>
2021-07-16 14:09:55 +0100

    DNRY

--- gli_py/glinet.py
+++ gli_py/glinet.py
@@ -17,6 +17,7 @@ from gli_py.error_handling import raise_for_status
 
 # typical base url http://192.168.8.1/cgi-bin/api/
 @timeout(2)
+@response_handler(raise_for_status)
 class GLinet(Consumer):
     """A Python Client for the GL-inet API."""
 
@@ -42,7 +43,6 @@ class GLinet(Consumer):
             else:
                 self.login(password)
 
-    @response_handler(raise_for_status)
     @returns.json(key="token")
     @post("router/login")
     def _login(self, pwd: Field):
@@ -68,31 +68,26 @@ class GLinet(Consumer):
             raise ConnectionRefusedError("Failed to authenticate with GL-inet. Error %s", err)
 
     # Basic device interaction
-    @response_handler(raise_for_status)
     @returns.json(key="model")
     @get("router/model")
     def router_model(self):
         """Retrieves the router's model, no auth required"""
 
-    @response_handler(raise_for_status)
     @returns.json(key="mac")
     @get("router/mac/get")
     def router_mac(self):
         """Retrieves the router's mac address"""
 
-    @response_handler(raise_for_status)
     @returns.json
     @get("router/status")
     def router_status(self):
         """router status"""
 
-    @response_handler(raise_for_status)
     @returns.json(key="has_new")
     @get("firmware/onlinecheck")
     def new_firmware(self):
         """whether there is new firmware to upgrade"""
 
-    @response_handler(raise_for_status)
     @returns.json
     @get("router/reboot")
     def reboot(self):
@@ -100,19 +95,16 @@ class GLinet(Consumer):
 
     # Basic WAN interaction
 
-    @response_handler(raise_for_status)
     @returns.json(key="ip")
     @get("wan/info")
     def wan_ip(self):
         """Retrieves the router's wan ip"""
 
-    @response_handler(raise_for_status)
     @returns.json(key="serverip")
     @get("internet/public_ip/get")
     def public_ip(self):
         """Retrieves the router's public ip. Will give VPN IP is connected"""
 
-    @response_handler(raise_for_status)
     @returns.json(key="reachable")
     @get("internet/reachable")
     def connected_to_internet(self):
@@ -120,13 +112,11 @@ class GLinet(Consumer):
 
     # Client information
 
-    @response_handler(raise_for_status)
     @returns.json(key="clients")
     @get("client/list")
     def list_all_clients(self):
         """gets all clients"""
 
-    @response_handler(raise_for_status)
     @returns.json(key="list")
     @get("router/static_leases/list")
     def list_static_clients(self):
@@ -142,7 +132,6 @@ class GLinet(Consumer):
 
     # VPN information
 
-    @response_handler(raise_for_status)
     @returns.json
     @get("wireguard/client/status")
     def wireguard_client_state(self):
@@ -151,7 +140,6 @@ class GLinet(Consumer):
     # SMS stuff
 
     # TODO untested
-    @response_handler(raise_for_status)
     @returns.json(key="modems")
     @get("modem/info")
     def _get_modems(self):
@@ -162,14 +150,12 @@ class GLinet(Consumer):
         return len(self._get_modems())
 
     # TODO untested
-    @response_handler(raise_for_status)
     @returns.json
     @get("modem/sms/status")
     def sms_status(self, modem_id: Field):
         """Retrieves the status of the SMS modem"""
 
     # TODO untested
-    @response_handler(raise_for_status)
     @returns.json
     @post("modem/sms/send")
     def _send_sms(self, modem_id: Field, message: Field, number: Field):
