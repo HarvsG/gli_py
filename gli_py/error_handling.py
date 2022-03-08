3f18eeb99921d948871b4498ccdb6a433fed94f5
HarvsG <11440490+HarvsG@users.noreply.github.com>
2022-03-08 23:19:10 +0000

    Revert to old handling

--- gli_py/error_handling.py
+++ gli_py/error_handling.py
@@ -14,22 +14,39 @@ class TokenError(Exception):
 
 
 def raise_for_status(response: Response):
-    def proc_res(response):
-        """Checks whether or not the response was successful."""
-        print(type(response))
-        print("sync response")
-        if 200 <= response.status_code < 300:
-            # Pass through the response.
-            res = loads(response.text)
-            # Gl-inet's api uses its own error codes that are returned in
-            # status 200 messages - this is out of spec so we must handle it
-            if res['code'] == -1:
-                raise TokenError("Request returned error code -1 (InvalidAuth), is the token expired or the passowrd wrong?")
-            if res['code'] < 0:
-                if 'msg' not in res:
-                    res['msg'] = "null"
-                raise NonZeroResponse("Request returned error code %s with message:' %s'" % (res['code'], res['msg']))
-            return res
+    if type(response) is not uplink.clients.aiohttp_.ThreadedResponse:
+        def proc_res(response):
+            """Checks whether or not the response was successful."""
+            print(type(response))
+            print("sync response")
+            if 200 <= response.status_code < 300:
+                # Pass through the response.
+                res = loads(response.text)
+                # Gl-inet's api uses its own error codes that are returned in
+                # status 200 messages - this is out of spec so we must handle it
+                if res['code'] == -1:
+                    raise TokenError("Request returned error code -1 (InvalidAuth), is the token expired or the passowrd wrong?")
+                if res['code'] < 0:
+                    if 'msg' not in res:
+                        res['msg'] = "null"
+                    raise NonZeroResponse("Request returned error code %s with message:' %s'" % (res['code'], res['msg']))
+                return res
+    else:
+        async def proc_res(response):
+            """Checks whether or not the response was successful."""
+            if 200 <= response.status < 300:
+                # Pass through the response.
+                res = loads(response.text())
+                # Gl-inet's api uses its own error codes that are returned in
+                # status 200 messages - this is out of spec so we must handle it
+                if res['code'] == -1:
+                    raise TokenError("Request returned error code -1 (InvalidAuth), is the token expired or the passowrd wrong?")
+                if res['code'] < 0:
+                    if 'msg' not in res:
+                        res['msg'] = "null"
+
+                    raise NonZeroResponse("Request returned error code %s with message:' %s'" % (res['code'], res['msg']))
+                return res
     return proc_res(response)
 
     raise UnsuccessfulRequest(response.url)
